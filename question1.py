0eb79b6920dd7184e132fe77ed8d59b3f3b64c68
Angel Gonzalez <angel.gonzalez@daviestech.org>
2020-10-08 20:05:30 +0000

    finished

---
+++ question1.py
@@ -0,0 +1,4 @@
+y = int(input("Enter a number: "))
+print(y + 1)
+print(y + 2)
+print(y + 3)
---
+++ question2.py
@@ -0,0 +1,4 @@
+dec1=float(input("Enter a Decimal: " ))
+dec2=float(input("Enter a Second Decimal: " ))
+dec3=float(input("Enter a Third Decimal: " ))
+print(dec1 + dec2 + dec3)
